d4e80e6cc90d932b7649cc612770930010156022
blackbernardom <bernardoclassea@gmail.com>
2023-08-03 22:38:51 -0300

    chore: ajuste final

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
-    type     = string
-    default  = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 
 
 variable "instance_type" {
-    type     = string
-    default  = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
